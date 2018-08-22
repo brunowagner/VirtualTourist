91277fca2559b561566ebcce2a63b48dcf729355
Bruno Wagner <brunowvmoraes@gmail.com>
2018-08-22 18:47:04 -0300

    Implements addPin in first MapViewController

--- VirtualTourist/Controller/MapViewController.swift
+++ VirtualTourist/Controller/MapViewController.swift
@@ -10,7 +10,7 @@ import CoreData
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController, MKMapViewDelegate {
+class MapViewController: UIViewController {
     
     @IBOutlet weak var mapView : MKMapView!
     
@@ -24,14 +24,58 @@ class MapViewController: UIViewController, MKMapViewDelegate {
             print ("Region resoursed!")
             print (region)
         }
+        
+        //add longPress (permite executar uma funcao apos "clicar" e "segurar" um local no mapa)
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(addPin))
+        mapView.addGestureRecognizer(longPress)
 
         
     }
     
-    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
-        Preferences.sharedInstance().region = mapView.region
-        Preferences.sharedInstance().save()
+    @objc func addPin(gestureRecognizer:UIGestureRecognizer){
+        let touchPoint = gestureRecognizer.location(in: mapView)
+        let newCoordinates = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = newCoordinates
+        mapView.addAnnotation(annotation)
     }
+    
+    // se fosse adicionar um pino com o endereço como título
+    @objc func addAnnotation(gestureRecognizer:UIGestureRecognizer){
+        if gestureRecognizer.state == UIGestureRecognizerState.began {
+            let touchPoint = gestureRecognizer.location(in: mapView)
+            let newCoordinates = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = newCoordinates
+            
+            CLGeocoder().reverseGeocodeLocation(CLLocation(latitude: newCoordinates.latitude, longitude: newCoordinates.longitude), completionHandler: {(placemarks, error) -> Void in
+                if error != nil {
+                    print("Reverse geocoder failed with error" + (error?.localizedDescription)!)
+                    return
+                }
+                
+                if (placemarks?.count)! > 0 {
+                    let pm = placemarks![0] as CLPlacemark
+                    
+                    // not all places have thoroughfare & subThoroughfare so validate those values
+                    if pm.thoroughfare != nil && pm.subThoroughfare != nil {
+                        annotation.title = pm.thoroughfare! + ", " + (pm.subThoroughfare)!
+                        annotation.subtitle = pm.subLocality
+                        self.mapView.addAnnotation(annotation)
+                        print(pm)
+                    }
+                }
+                else {
+                    annotation.title = "Unknown Place"
+                    self.mapView.addAnnotation(annotation)
+                    print("Problem with the data received from geocoder")
+                }
+                //places.append(["name":annotation.title,"latitude":"\(newCoordinates.latitude)","longitude":"\(newCoordinates.longitude)"])
+            })
+        }
+    }
+    
+
 
 
     
@@ -53,4 +97,35 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         navigationController?.pushViewController(photosViewController!, animated: true)
     }
 
+}
+
+extension MapViewController: MKMapViewDelegate{
+    // MARK: - MKMapViewDelegate
+    
+    // Here we create a view with a "right callout accessory view". You might choose to look into other
+    // decoration alternatives. Notice the similarity between this method and the cellForRowAtIndexPath
+    // method in TableViewDataSource.
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        return pinView
+    }
+    
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        Preferences.sharedInstance().region = mapView.region
+        Preferences.sharedInstance().save()
+    }
+    
+    
 }
