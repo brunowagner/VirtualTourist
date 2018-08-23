51196ad11318de9cbc461719aa559570f92a84a6
Bruno Wagner <brunowvmoraes@gmail.com>
2018-08-23 11:26:58 -0300

    Configure mapView and pin at photoViewController.

--- VirtualTourist/Controller/PhotosViewController.swift
+++ VirtualTourist/Controller/PhotosViewController.swift
@@ -14,21 +14,23 @@ class PhotosViewController: UIViewController {
     var annotation : MKAnnotation!
     
     @IBOutlet weak var mapView : MKMapView!
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        mapView.addAnnotation(annotation)
-        let span = MKCoordinateSpanMake(0.1, 0.1)
-        let region = MKCoordinateRegionMake(annotation.coordinate, span)
-        mapView.region = region
+        addPin()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    fileprivate func addPin() {
+        mapView.delegate = self
+        mapView.addAnnotation(annotation)
+        let span = MKCoordinateSpanMake(0.001, 0.001)
+        let region = MKCoordinateRegionMake(annotation.coordinate, span)
+        mapView.region = region
     }
     
 
@@ -43,6 +45,24 @@ class PhotosViewController: UIViewController {
     */
 
 }
-//extension PhotosViewController: MKMapViewDelegate{
-//
-//}
+extension PhotosViewController: MKMapViewDelegate{
+    // Here we create a view with a "right callout accessory view". You might choose to look into other
+    // decoration alternatives. Notice the similarity between this method and the cellForRowAtIndexPath
+    // method in TableViewDataSource.
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = false
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        return pinView
+    }
+}
